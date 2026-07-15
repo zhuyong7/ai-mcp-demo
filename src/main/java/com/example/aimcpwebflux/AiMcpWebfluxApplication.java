e9ba81b200d5f23c72879b009dae91bd786405d9
zhuyong7 <792152117@qq.com>
2026-07-15 23:24:34 +0800

    处理依赖

--- src/main/java/com/example/aimcpwebflux/AiMcpWebfluxApplication.java
+++ src/main/java/com/example/aimcpwebflux/AiMcpWebfluxApplication.java
@@ -3,7 +3,7 @@ package com.example.aimcpwebflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ai")
 public class AiMcpWebfluxApplication {
 
     public static void main(String[] args) {
